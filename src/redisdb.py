0a35c89c09c89f77f756d82b6513551667df1ae0
Nguyen Duc Hoang <nguyenduchoang@hoangks5.local>
2023-10-09 10:10:09 +0700

    added function to check whether an id_crawl exists in a set_name in redisdb in redisdb.py

--- src/redisdb.py
+++ src/redisdb.py
@@ -18,6 +18,15 @@ def delete_id_crawl(id_crawl,set_name):
    # xoá id_crawl trong set_name
    redisdb.srem(set_name, id_crawl)
    
+def check_id_crawl_exist(id_crawl,set_name):
+   # nếu id_crawl đã tồn tại trong set_name trả về True
+   if redisdb.sismember(set_name, id_crawl) == 1:
+      return True
+   else:
+      return False
+
+
+   
    
    
 
