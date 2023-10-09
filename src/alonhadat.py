32fcfaa63d89def6ab528284f4a2ae14ad153d6e
hoangks5 <hoangnd.ute@gmail.com>
2023-10-09 10:39:07 +0700

    added proxy support to the requests.get in alonhadat.py file

--- src/alonhadat.py
+++ src/alonhadat.py
@@ -7,13 +7,17 @@ import redisdb
 
 mongodb = mongodb.MongoDB('tindangbatdongsan', 'raw')
 
+with open('proxy.txt', 'r') as f:
+   proxy = f.read()
+   
+   
 def getPage(offset):
    if offset == 0 or offset ==1:
       url = 'https://alonhadat.com.vn/nha-dat/can-ban.html'
    else:
       url = f'https://alonhadat.com.vn/nha-dat/can-ban/trang--{offset}.html'
       
-   response = requests.get(url)
+   response = requests.get(url, proxies={'https': proxy})
    soup = BeautifulSoup(response.text, 'html.parser')
    links = soup.find_all('div', class_='ct_title')
    links = [link.find('a')['href'] for link in links]
@@ -22,7 +26,7 @@ def getPage(offset):
    return links
    
 def getHTML(url):
-   response = requests.get(url)
+   response = requests.get(url, proxies={'https': proxy})
    return {'id_crawl': hashlib.md5(url.encode()).hexdigest(), 'website': 'alonhadat.com.vn', 'data': response.text}
 
 def run(offset):
