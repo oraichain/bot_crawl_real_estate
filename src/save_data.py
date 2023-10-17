9fcbf5be700fd45c50f253d7520b9a7cfb9b4588
Nguyen Duc Hoang <hoangnd.ute@gmail.com>
2023-10-17 16:14:27 +0700

    commit message: added function to save data

--- src/save_data.py
+++ src/save_data.py
@@ -1,2 +1,12 @@
 import os
+from datetime import datetime
 
+
+
+def save(hashmd5,data):
+   xpath = './raw/'+ datetime.now().strftime('%Y-%m-%d')
+   if not os.path.exists(xpath):
+      os.mkdir(xpath)
+   if not os.path.exists(f'{xpath}/{hashmd5}'):
+      with open(f'{xpath}/{hashmd5}','w',encoding='utf-8') as f:
+         f.write(data)
