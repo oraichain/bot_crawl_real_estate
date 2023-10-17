c9165d2dd5edd2330e73de29d4ca8f2af16af45b
Nguyen Duc Hoang <hoangnd.ute@gmail.com>
2023-10-17 16:20:22 +0700

    fix: save data function for multiple websites
    giải thích: commit này sửa lỗi phần thêm dữ liệu khác nhau cho nhiều websites. cụ thể, code đã được thay đổi để tạo đường dẫn cho từng website trước khi lưu dữ liệu

--- src/save_data.py
+++ src/save_data.py
@@ -2,11 +2,11 @@ import os
 from datetime import datetime
 
 
-
-def save(hashmd5,data):
-   xpath = './raw/'+ datetime.now().strftime('%Y-%m-%d')
+def save(hashmd5,data,website):
+   xpath = f'./raw/{website}/{datetime.now().strftime("%Y-%m-%d")}'
    if not os.path.exists(xpath):
       os.mkdir(xpath)
    if not os.path.exists(f'{xpath}/{hashmd5}'):
-      with open(f'{xpath}/{hashmd5}','w',encoding='utf-8') as f:
+      with open(f'{xpath}/{hashmd5}', 'w') as f:
          f.write(data)
+         f.close()
