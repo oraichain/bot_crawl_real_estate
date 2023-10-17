3e2153b3fb371c19953926150056ad45312bbb92
Nguyen Duc Hoang <hoangnd.ute@gmail.com>
2023-10-17 16:30:12 +0700

    update folder with domain

--- src/save_data.py
+++ src/save_data.py
@@ -2,11 +2,12 @@ import os
 from datetime import datetime
 
 
+
 def save(hashmd5,data,website):
    xpath = f'./raw/{website}/{datetime.now().strftime("%Y-%m-%d")}'
    if not os.path.exists(xpath):
       os.mkdir(xpath)
    if not os.path.exists(f'{xpath}/{hashmd5}'):
-      with open(f'{xpath}/{hashmd5}', 'w') as f:
+      with open(f'{xpath}/{hashmd5}','w',encoding='utf-8') as f:
          f.write(data)
          f.close()
