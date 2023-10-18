7900fd81b86d2e7fb4e4d1049ba60b8d56f1782f
Nguyen Duc Hoang <hoangnd.ute@gmail.com>
2023-10-18 17:17:47 +0700

    added proxy configuration for scraping

--- src/batdongsan.py
+++ src/batdongsan.py
@@ -4,16 +4,12 @@ from bs4 import BeautifulSoup
 import hashlib
 import threading
 import mongodb
-from utils import logging
+from utils import logging, get_proxy
 import redisdb
-import requests
-from webdriver_manager.chrome import ChromeDriverManager
 from dotenv import load_dotenv
 load_dotenv()
-import os
 mongodb = mongodb.MongoDB('tindangbatdongsan', 'raw')
-
-
+PROXY = get_proxy(15*60)
 
 def create_driver(page):         
     options = uc.ChromeOptions()
@@ -23,17 +19,13 @@ def create_driver(page):
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
+    # fixed detection automation headless by cloudflare
+    options.add_argument('--headless')
     options.add_argument('--disable-extensions')
-    # tắt thông báo
     options.add_argument("--disable-notifications")
-    # add proxy 116.99.117.216:56753 vào chrome
-    with open('proxy.txt','r') as f:
-        proxy = f.read()
-    options.add_argument('--proxy-server='+proxy)
+    options.add_argument('--proxy-server='+PROXY)
     driver = uc.Chrome(options=options,headless=False,version_main=117)
-    # set size cho chrome
     driver.set_window_size(320, 360)
-    # set vị trí cho chrome
     driver.set_window_position((page-1)//3*320, (page-1)%3*360)
     return driver
 
@@ -49,12 +41,13 @@ def getPage(driver,page):
     except:
         return []
     time.sleep(30)
-    driver.save_screenshot('batdongsan.png')
+    driver.save_screenshot(f'./src/batdongsan.com.vn.png')
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     # lấy tất cả link trong trang
     links = soup.find_all('a', class_='js__product-link-for-product-id')
     links = ['https://batdongsan.com.vn'+link['href'] for link in links]
+    print(links)
     return links
 
 
@@ -63,7 +56,6 @@ def getHTML(driver,url):
         driver.set_page_load_timeout(10)
         try:
             driver.get(url)
-            driver.save_screenshot('batdongsan.png')
         except:
             redisdb.delete_id_crawl(hashlib.md5(url.encode()).hexdigest(),'raw')
             return None
@@ -87,7 +79,7 @@ def crawl_one_thread(page):
         
 def process():
       threads = []
-      for i in range(1, 11):
+      for i in range(1, 2):
          threads.append(threading.Thread(target=crawl_one_thread, args=(i,)))
       for thread in threads:
          thread.start()
