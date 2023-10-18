7dcb72b1febbb0791abc123bb0b915f89f078712
Nguyen Duc Hoang <hoangnd.ute@gmail.com>
2023-10-18 17:20:55 +0700

    update homedy crawler, import logging utils

--- src/homedy.py
+++ src/homedy.py
@@ -2,17 +2,16 @@
 import requests
 from bs4 import BeautifulSoup
 import mongodb
-from noti_logging import logging
+from utils import logging
 import hashlib
 import redisdb
 
 
 mongodb = mongodb.MongoDB('tindangbatdongsan', 'raw')
 
+
 def getPage(page):
-
   url = f"https://homedy.com/ban-nha-dat/p{page}?sort=new"
-
   payload={}
   headers = {
     'authority': 'mogi.vn',
@@ -36,31 +35,29 @@ def getPage(page):
   links = [f'https://homedy.com{link["href"]}' for link in links]
   return links
    
+   
 def getHTML(url):
+  payload={}
+  headers = {
+  'authority': 'homedy.com',
+  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+  'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
+  'cookie': '_source_utm=organic; _source_campaign=default; _source_referrer=google; _gid=GA1.2.754350581.1693218818; userId=fb0a923f-aa3e-40e8-8e24-e5b14a4077a8; UserIdHomedyShare=fb0a923f-aa3e-40e8-8e24-e5b14a4077a8; _fbp=fb.1.1693218819207.1964814849; __gads=ID=561c1723827dd09a-229213b328e3004e:T=1693218822:RT=1693218822:S=ALNI_MZy7NcFY0yTdxPYQxZAghUjBr1nxg; __gpi=UID=00000c351c1d3fa6:T=1693218822:RT=1693218822:S=ALNI_MZKmA7J8AJjsAVty9viT79t0KFXPw; _ga_3TM6MNV523=GS1.1.1693218818.1.1.1693218848.30.0.0; _ga=GA1.1.1023301757.1693218818; _ga_W67931SQGN=GS1.2.1693218818.1.1.1693218864.14.0.0; .AspNetCore.Antiforgery.qKMI6Lrj50Q=CfDJ8PyCrl482c5Pv2aBwHCe-CxJx2gD3ptNY18vGsQhOkVoLPUd58mjigXklS4sOgpk1nBQhc9iwtflniSY4-GE5b_KJ3ezm-g12ma5kOFHJmX_IjaghPAQvyqDsGXGhQG-YmDcdir22GDAjpht9XpfJKk',
+  'referer': 'https://homedy.com/ban-nha-dat/p7',
+  'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"macOS"',
+  'sec-fetch-dest': 'document',
+  'sec-fetch-mode': 'navigate',
+  'sec-fetch-site': 'same-origin',
+  'sec-fetch-user': '?1',
+  'upgrade-insecure-requests': '1',
+  'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+  }
+  response = requests.request("GET", url, headers=headers, data=payload)
+  website = url.split("/")[2]
+  return {'id_crawl': hashlib.sha256(url.encode()).hexdigest(),'website': website, 'data': response.text}
 
-   payload={}
-   headers = {
-   'authority': 'homedy.com',
-   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-   'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
-   'cookie': '_source_utm=organic; _source_campaign=default; _source_referrer=google; _gid=GA1.2.754350581.1693218818; userId=fb0a923f-aa3e-40e8-8e24-e5b14a4077a8; UserIdHomedyShare=fb0a923f-aa3e-40e8-8e24-e5b14a4077a8; _fbp=fb.1.1693218819207.1964814849; __gads=ID=561c1723827dd09a-229213b328e3004e:T=1693218822:RT=1693218822:S=ALNI_MZy7NcFY0yTdxPYQxZAghUjBr1nxg; __gpi=UID=00000c351c1d3fa6:T=1693218822:RT=1693218822:S=ALNI_MZKmA7J8AJjsAVty9viT79t0KFXPw; _ga_3TM6MNV523=GS1.1.1693218818.1.1.1693218848.30.0.0; _ga=GA1.1.1023301757.1693218818; _ga_W67931SQGN=GS1.2.1693218818.1.1.1693218864.14.0.0; .AspNetCore.Antiforgery.qKMI6Lrj50Q=CfDJ8PyCrl482c5Pv2aBwHCe-CxJx2gD3ptNY18vGsQhOkVoLPUd58mjigXklS4sOgpk1nBQhc9iwtflniSY4-GE5b_KJ3ezm-g12ma5kOFHJmX_IjaghPAQvyqDsGXGhQG-YmDcdir22GDAjpht9XpfJKk',
-   'referer': 'https://homedy.com/ban-nha-dat/p7',
-   'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-   'sec-ch-ua-mobile': '?0',
-   'sec-ch-ua-platform': '"macOS"',
-   'sec-fetch-dest': 'document',
-   'sec-fetch-mode': 'navigate',
-   'sec-fetch-site': 'same-origin',
-   'sec-fetch-user': '?1',
-   'upgrade-insecure-requests': '1',
-   'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
-   }
-
-   response = requests.request("GET", url, headers=headers, data=payload)
-
-   website = url.split("/")[2]
-
-   return {'id_crawl': hashlib.sha256(url.encode()).hexdigest(),'website': website, 'data': response.text}
 
 def run(offset):
   for i in range(1,offset):
@@ -71,4 +68,5 @@ def run(offset):
           mongodb.insert(data)
           logging(f'Crawled website: homedy.com, Id: {data["id_crawl"]}, Link: {link}')  
   mongodb.close()
+  
 run(2)
