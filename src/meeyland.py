9556b4580d49cf55e752c336bf7d789e013e34f4
Nguyen Duc Hoang <hoangnd.ute@gmail.com>
2023-10-18 14:23:53 +0700

    added meeyland public key

--- src/meeyland.py
+++ src/meeyland.py
@@ -7,9 +7,22 @@ import redisdb
 import hashlib
 
 mongodb = mongodb.MongoDB('tindangbatdongsan', 'raw')
+with open('./src/public_key_meeyland', 'r') as f:
+   PUBLIC_KEY = f.read()
+def get_key():
+   url = 'https://meeyland.com/mua-ban-nha-dat'
+   response = requests.get(url)
+   if response.status_code != 200:
+      return get_key()
+   soup = BeautifulSoup(response.text, 'html.parser')
+   scripts = soup.find_all('script')
+   for script in scripts:
+      if '_buildManifest.js' in str(script.get('src')):
+         return script.get('src').split('/')[5]
+      
 
 def crawlNewFeed():
-   url = 'https://meeyland.com/_next/data/J6jtKP9iWZmX9RomarPa3/mua-ban-nha-dat.json?filter=need%5B%5D%3Dcan_ban&sort=0&category=mua-ban-nha-dat'
+   url = f'https://meeyland.com/_next/data/{PUBLIC_KEY}/mua-ban-nha-dat.json?filter=need%5B%5D%3Dcan_ban&sort=0&category=mua-ban-nha-dat'
    response = requests.get(url)
    if response.status_code != 200:
       return crawlNewFeed()
