8096926c07297285d75b2601bd53aeabc5781b33
Nguyen Duc Hoang <hoangnd.ute@gmail.com>
2023-10-18 17:30:04 +0700

    add redis client and functions

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import requests
 import time
 from dotenv import load_dotenv
+import redis
 load_dotenv()
 
 
@@ -57,4 +58,40 @@ def get_proxy(time_live_proxy):
          return get_proxy(time_live_proxy)
       else:
          return info_proxy[0]
+      
+
+
+def client():
+   redis_host = 'localhost' 
+   redis_port = 6379
+   redis_password = None
+   redisdb = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+   return redisdb
+
+
+def check_id_crawl(id_crawl,set_name):
+   # nếu id_crawl chưa tồn tại trong set_name trả về True
+   client_ = client()
+   status = client_.sadd(set_name, id_crawl)
+   client_.close()
+   if status == 1:
+      return True
+   else:
+      return False
+
+
+def delete_id_crawl(id_crawl,set_name):
+   client_ = client()
+   client_.srem(set_name, id_crawl)
+   client_.close()
+   
+   
+def check_id_crawl_exist(id_crawl,set_name):
+   client_ = client()
+   status = client_.sismember(set_name, id_crawl)
+   client_.close()
+   if status == 1:
+      return True
+   else:
+      return False
       
